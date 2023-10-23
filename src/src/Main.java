1d9d2074b317a62c881c99405001030847fe1bab
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-23 13:33:19 +0200

    factorial

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner leer=new Scanner(System.in);
+        int NUM,DIV,FACT=0;
+        boolean Primo=false;
         {
             //con el scaner cogeis un numero entero positivo y una cadena
 
@@ -11,6 +15,19 @@ public class Main {
             //ver cuantas mayusculas tiene la cadena
 
             //Si no es primo factorizarlo:
+            System.out.println("NUM");
+            NUM=leer.nextInt();
+            while (Primo=false){
+                for (DIV=1;DIV<=NUM;NUM++){
+                    if(NUM%DIV==0){
+                        System.out.println(+DIV+"es divisor de"+NUM);
+                        FACT=FACT*DIV;
+                    }
+
+                }
+                System.out.println("el factorial de"+NUM+"es"+FACT);
+            }
+
 
             //mostrar el tamaño de la cadena
         }
