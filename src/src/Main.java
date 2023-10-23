cbcab73ced12c35a80dcc0ad7259c8315e880acd
JaimeManero <jmaneromo23de@ikzubirimanteo.com>
2023-10-23 12:30:41 +0200

    1

---
+++ src/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        {
+            //con el scaner cogeis un numero entero positivo y una cadena
+
+            //ver el string es palindromo
+            //[
+            //ver si el numero es primo
+
+            //ver cuantas mayusculas tiene la cadena
+
+            //Si no es primo factorizarlo:
+
+            //mostrar el tamaño de la cadena
+        }
+    }
+}
